8c38e7526a0be47ecda0d20453126c96127a2b0c
Feizan Ulhaq <u1474090@unimail.hud.ac.uk>
2017-01-16 15:58:22 +0000

    fix to calc itemprice based on quantity

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -12,7 +12,7 @@
 
 @foreach($items as $item)
 
-<h4>{{ $item->itemName}} -  {{ $item->price }}<p style="float:right"> {{ $item->quantity}} </h4>
+<h4>{{ $item->itemName}} -  {{ $item->price * $item->quantity }}<p style="float:right"> {{ $item->quantity}} </h4>
 <a href="{{route('items.show', $item->id) }}" class="btn btn-primary btn-sm">View Item</a>
 <a href="{{route('items.edit', $item->id) }}" class="btn btn-primary btn-sm">Edit Item</a>
 <hr>
